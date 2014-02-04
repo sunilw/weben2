6a9930be8fa856427b0badcad99b9a1dcf50f8ab
Sunil Williams <sunil@sunil.co.nz>
2014-02-04 20:07:21 +1300

    add colours, add cabin font

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt){
+
+    grunt.initConfig({
+
+        compass : {
+            dist : {
+                options : {
+                    cssDir : 'css',
+                    sassDir : 'sass',
+                    debugInfo : true
+                }
+            }
+        },
+
+        connect : {
+            uses_defaults : {}
+        },
+
+        watch : {
+            files : ['*.html', 'js/*', 'sass/*', 'css/*'],
+            tasks : ['compass'],
+            options : {
+                livereload: true
+            }
+        }
+
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+
+    
+    grunt.registerTask('default', ['watch'])
+
+} ;
